21909429251469d1fca35b6aba4d4a9ab3b1b2f9
Ayan Rakhimov <143061005+Dr-Drazzat@users.noreply.github.com>
2026-03-25 21:18:34 +0500

    Ayan Rakhimov 1 task

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int sumOfSquares(int n) {
+        //Time complexity is O(n), function call itself n times from n to 1
+        //Space complexity is O(n) each recursive call takes place in stack, starting from n to 1
+        if (n == 1) return 1;
+        return n * n + sumOfSquares(n - 1);
+    }
+
+}
